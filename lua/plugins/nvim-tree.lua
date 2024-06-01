a7aa1726cb1eb630e709db22a647c0e18f06042b
skdltmxn <supershop@naver.com>
2024-06-02 01:44:34 +0900

    fix some settings

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -24,7 +24,7 @@ return {
 				actions = {
 					open_file = {
 						window_picker = {
-							enable = false,
+							enable = true,
 						},
 					},
 				},
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,35 +1,35 @@
 return {
-    {
-        "nvim-telescope/telescope.nvim",
-        tag = "0.1.3",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-            "nvim-tree/nvim-web-devicons",
-        },
-        config = function()
-            local telescope = require("telescope")
-            local actions = require("telescope.actions")
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			local telescope = require("telescope")
+			local actions = require("telescope.actions")
 
-            telescope.setup({
-                defaults = {
-                    path_display = { "smart" },
-                    mappings = {
-                        i = {
-                            ["<esc>"] = actions.close,
-                            ["<C-j>"] = actions.move_selection_next,
-                            ["<C-k>"] = actions.move_selection_previous,
-                        },
-                    },
-                },
-            })
+			telescope.setup({
+				defaults = {
+					path_display = { "truncate" },
+					mappings = {
+						i = {
+							["<esc>"] = actions.close,
+							["<C-j>"] = actions.move_selection_next,
+							["<C-k>"] = actions.move_selection_previous,
+						},
+					},
+				},
+			})
 
-            telescope.load_extension("fzf")
-        end,
-        keys = {
-            { "<leader>sf", "<cmd>Telescope find_files<cr>", desc = "Search Files" },
-            { "<leader>so", "<cmd>Telescope oldfiles<cr>",   desc = "Search Recent Files" },
-            { "<leader>sg", "<cmd>Telescope live_grep<cr>",  desc = "String Grep" },
-        }
-    }
+			telescope.load_extension("fzf")
+		end,
+		keys = {
+			{ "<leader>sf", "<cmd>Telescope find_files<cr>", desc = "Search Files" },
+			{ "<leader>so", "<cmd>Telescope oldfiles<cr>", desc = "Search Recent Files" },
+			{ "<leader>sg", "<cmd>Telescope live_grep<cr>", desc = "String Grep" },
+		},
+	},
 }
